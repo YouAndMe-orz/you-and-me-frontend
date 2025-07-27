38c9c6f56aba2a8f9351955f23b228ab62ca04cf
nno3onn <nno3onn@naver.com>
2025-07-27 23:43:57 +0900

    chore: 프로젝트 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@/styles/globals.css";
+
+import QueryClientProvider from "./QueryClientProvider";
+import NextAuthProvider from "./NextAuthProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="ko">
+      <body className="mx-auto flex max-w-[600px] flex-col">
+        <NextAuthProvider>
+          <QueryClientProvider>
+            {children}
+            <ReactQueryDevtools />
+          </QueryClientProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
